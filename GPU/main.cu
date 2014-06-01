03988294876432ee17706e1c3a001628804fa867
Raymond van Venetie <rvveneti@fs3.cm.cluster>
2014-06-01 23:40:23 +0200

    Small changes

--- GPU/main.cu
+++ GPU/main.cu
@@ -39,7 +39,7 @@ __global__ void square_array(float *a, int N)
   if (idx<N) a[idx] = a[idx] * a[idx];
 }
 
-__global__ void tet_acute_kernel(triangle triang, int dim, unsigned char * result, size_t N) {
+__global__ void tet_acute_kernel(ptriangle triang, int dim, unsigned char * result, size_t N) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx>= N)
     return;
@@ -52,11 +52,11 @@ __global__ void tet_acute_kernel(triangle triang, int dim, unsigned char * resul
   cur_pt[1] = t / (dim + 1);
   cur_pt[2] = t % (dim + 1);
   
-  subArr3(triang.vertices[1], triang.vertices[0], P[0]);
-  subArr3(triang.vertices[2], triang.vertices[0], P[1]);
-  subArr3(cur_pt, triang.vertices[0], P[2]);
-  subArr3(triang.vertices[2], triang.vertices[1], P[3]);
-  subArr3(cur_pt, triang.vertices[1], P[4]); 
+  subArr3(triang->vertices[1], triang->vertices[0], P[0]);
+  subArr3(triang->vertices[2], triang->vertices[0], P[1]);
+  subArr3(cur_pt, triang->vertices[0], P[2]);
+  subArr3(triang->vertices[2], triang->vertices[1], P[3]);
+  subArr3(cur_pt, triang->vertices[1], P[4]); 
   
   crossArr3(P[2],P[0], normals[2]); //Normal on facet 0,1,3
   crossArr3(P[0],P[1], normals[3]); //Normal on facet 0,1,2
@@ -88,30 +88,29 @@ triangle rand_triangle(int dim) {
 }
 
 #define DIM 150
-#define THREADS_BLOCK 1024
+#define THREADS_BLOCK 512
 int main(void)
 {
   clock_t begin, end;
   cudaEvent_t start, stop;
   float time_ms;
   triangle triang = rand_triangle(DIM);
+  triangle * ptriang;
   arr3 dim = {DIM,DIM,DIM};
   cube_points cube_pts = gen_cube_points(dim);
   arr3 * cube_d;
   unsigned char * res_h, *res_d;
   timeval t1,t2;
-  gettimeofday(&t1,NULL);
-  begin = clock();
   cudaEventCreate(&start);
   cudaEventCreate(&stop);
+  gettimeofday(&t1,NULL);
+  begin = clock();
   checkCudaCall(cudaMalloc(&res_d, cube_pts.len * sizeof(unsigned char)));
-  
+  checkCudaCall(cudaMalloc(&ptriang, sizeof(triangle))); 
   res_h = (unsigned char *) malloc(cube_pts.len * sizeof(unsigned char));
-  for (int i = 0; i < 5; i++)
-    printf("%d %d\n", i, res_h[i]);
-  
+  checkCudaCall(cudaMemcpy(ptriang, &triang, sizeof(triangle), cudaMemcpyHostToDevice));
   cudaEventRecord(start,0);
-  tet_acute_kernel <<< cube_pts.len/THREADS_BLOCK + 1  , THREADS_BLOCK >>> (triang, DIM, res_d, cube_pts.len);
+  tet_acute_kernel <<< cube_pts.len/THREADS_BLOCK + 1  , THREADS_BLOCK >>> (ptriang, DIM, res_d, cube_pts.len);
   cudaEventRecord(stop,0);  
   checkCudaCall(cudaGetLastError());
   checkCudaCall(cudaMemcpy(res_h, res_d, cube_pts.len * sizeof(unsigned char), cudaMemcpyDeviceToHost));
