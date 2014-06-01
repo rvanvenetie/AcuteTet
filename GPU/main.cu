b7fb344e06bd13605c11c268fc17b78634d3607c
Raymond van Venetie <rayvanve@gmail.com>
2014-06-01 19:39:44 +0200

    Test for CUDA

--- GPU/main.cu
+++ GPU/main.cu
@@ -1,10 +1,34 @@
 // example1.cpp : Defines the entry point for the console application.
 //
- 
+
 #include <stdio.h>
+#include <iostream>
 #include <cuda.h>
-#include "../vector.h" 
+extern "C" {
+#include "../vector.h"
+#include "../triangle.h"  
 #include "../tetraeder.h"
+  
+}
+using namespace std;
+
+/* Utility function, use to do error checking.
+
+   Use this function like this:
+
+   checkCudaCall(cudaMalloc((void **) &deviceRGB, imgS * sizeof(color_t)));
+
+   And to check the result of a kernel invocation:
+
+   checkCudaCall(cudaGetLastError());
+*/
+static void checkCudaCall(cudaError_t result) {
+    if (result != cudaSuccess) {
+        cerr << "cuda error: " << cudaGetErrorString(result) << endl;
+        exit(1);
+    }
+}
+
 // Kernel that executes on the CUDA device
 __global__ void square_array(float *a, int N)
 {
@@ -12,27 +36,86 @@ __global__ void square_array(float *a, int N)
   if (idx<N) a[idx] = a[idx] * a[idx];
 }
 
+__global__ void tet_acute_kernel(int * result) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  result[idx] = 12345;
+  /*
+  return;
+  if (idx>= N)
+    return;
+  
+  arr3 P[5]; 
+  arr3 normals[4]; 
+  subArr3(triang.vertices[1], triang.vertices[0], P[0]);
+  subArr3(triang.vertices[2], triang.vertices[0], P[1]);
+  subArr3(triang.vertices[2], triang.vertices[1], P[3]);
+  subArr3(new_vec[idx], triang.vertices[1], P[4]); 
+  subArr3(new_vec[idx], triang.vertices[0], P[2]);
+  
+  crossArr3(P[2],P[0], normals[2]); //Normal on facet 0,1,3
+  crossArr3(P[0],P[1], normals[3]); //Normal on facet 0,1,2
+  crossArr3(P[1],P[2], normals[1]); //Normal on facet 0,2,3
+  crossArr3(P[4],P[3], normals[0]); //Normal on facet 1,2,3
 
-//__global__ void tetra_acute( 
-// main routine that executes on the host
+  result[idx] = (dotArr3(normals[1], normals[2]) < 0 &&
+                 dotArr3(normals[1], normals[3]) < 0 &&
+                 dotArr3(normals[0], normals[1]) < 0 &&
+                 dotArr3(normals[0], normals[2]) < 0 &&
+                 dotArr3(normals[0], normals[3]) < 0);
+  */
+}
+
+
+
+triangle rand_triangle(int dim) {
+  triangle result;
+  result.vertices[0][0] = rand() % dim;
+  result.vertices[0][1] = rand() % dim;
+  result.vertices[0][2] = rand() % dim;
+  result.vertices[1][0] = rand() % dim;
+  result.vertices[1][1] = rand() % dim;
+  result.vertices[1][2] = rand() % dim;
+  result.vertices[2][0] = rand() % dim;
+  result.vertices[2][1] = rand() % dim;
+  result.vertices[2][2] = rand() % dim;
+  return result;
+}
+
+#define DIM 7
+
 int main(void)
 {
-  float *a_h, *a_d;  // Pointer to host & device arrays
-  const int N = 10;  // Number of elements in arrays
-  size_t size = N * sizeof(float);
-  a_h = (float *)malloc(size);        // Allocate array on host
-  cudaMalloc((void **) &a_d, size);   // Allocate array on device
-  // Initialize host array and copy it to CUDA device
-  for (int i=0; i<N; i++) a_h[i] = (float)i;
-  cudaMemcpy(a_d, a_h, size, cudaMemcpyHostToDevice);
-  // Do calculation on device:
-  int block_size = 4;
-  int n_blocks = N/block_size + (N%block_size == 0 ? 0:1);
-  square_array <<< n_blocks, block_size >>> (a_d, N);
-  // Retrieve result from device and store it in host array
-  cudaMemcpy(a_h, a_d, sizeof(float)*N, cudaMemcpyDeviceToHost);
-  // Print results
-  for (int i=0; i<N; i++) printf("%d %f\n", i, a_h[i]);
-  // Cleanup
-  free(a_h); cudaFree(a_d);
+  triangle triang = rand_triangle(DIM);
+  arr3 dim = {DIM,DIM,DIM};
+  cube_points cube_pts = gen_cube_points(dim);
+  
+  arr3 * cube_d;
+  int * res_h, *res_d;
+  
+  checkCudaCall(cudaMalloc(&cube_d, cube_pts.len * sizeof(arr3)));
+  checkCudaCall(cudaMalloc(&res_d, cube_pts.len * sizeof(int)));
+  
+  res_h = (int *) malloc(cube_pts.len * sizeof(int));
+  for (int i = 0; i < 5; i++)
+    printf("%d %d\n", i, res_h[i]);
+  
+  checkCudaCall(cudaMemcpy(cube_d, &cube_pts.points[0], cube_pts.len * sizeof(arr3), cudaMemcpyHostToDevice));
+  
+  tet_acute_kernel <<< 1, 256 >>> (res_d);
+  
+  checkCudaCall(cudaGetLastError());
+  checkCudaCall(cudaMemcpy(res_h, res_d, cube_pts.len * sizeof(int), cudaMemcpyDeviceToHost));
+
+  for (int i=0; i<5; i++) {
+    printf("Cuda: %d %d\n", i, res_h[i]);
+    tetra test_tetra;
+    memcpy(test_tetra.vertices, &cube_pts.points[i], sizeof(arr3));
+    memcpy(test_tetra.vertices + 1, triang.vertices, 3 * sizeof(arr3)); 
+    printf("Normal: %d %d\n", i , tetra_acute(&test_tetra));
+  }
+   
+  cudaFree(cube_d);
+  cudaFree(res_d);
+  free(cube_pts.points);
+  free(res_h);
 }
